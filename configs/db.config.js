7efffd78f42b36c6ac05719f948f30fa0e2d198b
priyajais1994 <kumaripriyanka271439@gmail.com>
2023-07-02 15:28:20 +0530

    imp changes

---
+++ configs/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DB_NAME:"mba_db",
+    DB_URL : "mongodb+srv://priyankajaiswal:13jan2018@cluster0.ctnvx4a.mongodb.net/?retryWrites=true&w=majority"
+    
+
+   // DB_URL:"mongodb+srv://utkarsh:qwerty123@cluster0.85op4lq.mongodb.net/?retryWrites=true&w=majority"
+}
